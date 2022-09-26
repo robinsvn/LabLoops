b195b0d6554b1f76c61269474e86d22d67d5d0ca
BuildTools <unconfigured@null.spigotmc.org>
2022-09-26 12:36:52 +0200

    LabLoops

--- LabLoops/Program.cs
+++ LabLoops/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,17 +18,17 @@ namespace LabLoops
 
             //*************while**************************
 
-            Console.WriteLine("while");
+            //Console.WriteLine("while");
 
             /* local variable definition */
-            int a = 10;
+            //int a = 10;
 
             /* while loop execution */
-            while (a < 20)
-            {
-                Console.WriteLine("value of a: {0}", a);
-                a++;
-            }
+            //while (a < 20)
+            //{
+            //Console.WriteLine("value of a: {0}", a);
+            //a++;
+            //}
             //denne går så lenge (while) er mindre enn 20. a++ betyr at a øker med 1 for hver "runde" 
 
 
@@ -35,43 +36,43 @@ namespace LabLoops
             //*************for**************************
 
             //I en for loop bestemmer du som programmerer start, stop og hvor mye den skal øke for hver runde
-            Console.WriteLine("for loop");
+            //Console.WriteLine("for loop");
 
             /* for loop execution */
-            for (int i = 10; i < 20; i++)//i øker med 1 for hver runde. 
-            {
-                Console.WriteLine("value of i: {0}", i);
-            }
+            //for (int i = 10; i < 20; i++)//i øker med 1 for hver runde. 
+            //{
+            //Console.WriteLine("value of i: {0}", i);
+            //}
 
             //Du kan også bruke variabler av typen int istedenfor å skirve 10 og 20
 
-            Console.WriteLine("for loop eks 2");
+            //Console.WriteLine("for loop eks 2");
 
-            int start = 10;
-            int stopp = 20;
+            //int start = 10;
+            //int stopp = 20;
 
-            for (int i = start; i < stopp; i++)//i øker med 1 for hver runde. 
-            {
-                Console.WriteLine("value of i: {0}", i);
-            }
+            //for (int i = start; i < stopp; i++)//i øker med 1 for hver runde. 
+            //{
+            //    Console.WriteLine("value of i: {0}", i);
+            //}
 
 
             //*************do while**************************
 
             //Dette er broren til while. Forskjellen er at betingelsen/testen dukker opp i slutten av loopen, slik at innholdet i loopen kjører en gang før testen blir gjennomført.
 
-            Console.WriteLine("do while");
+            //Console.WriteLine("do while");
 
             /* local variable definition */
-            int dw = 10;
+            //int dw = 10;
 
             /* do loop execution */
-            do
-            {
-                Console.WriteLine("value of dw: {0}", dw);
-                dw = dw + 1;//det samme som dw++;
-            }
-            while (dw < 20);
+            //do
+            //{
+            //Console.WriteLine("value of dw: {0}", dw);
+            //dw = dw + 1;//det samme som dw++;
+            //}
+            //while (dw < 20);
 
 
             //*************foreach**************************
@@ -79,26 +80,26 @@ namespace LabLoops
             //Denne loopen går igjennom elementer eller variabler. For hvert element.... Har du 5 elementer går den 5 ganger
             //Vi tester med å lage en array. Loopen vil gå igjennom arrayen i den rekkefølgen det er.
 
-            Console.WriteLine("foreach");
+            //Console.WriteLine("foreach");
 
             // creating an array 
-            int[] array = new int[] { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+            //int[] array = new int[] { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
 
             // foreach loop begin 
             // it will run till the 
             // last element of the array 
-            foreach (int item in array)
-            {
-                Console.WriteLine("value of item: " + item);
-            }
+            //foreach (int item in array)
+            //{
+            //Console.WriteLine("value of item: " + item);
+            //}
 
-            Console.WriteLine("foreach eksempel 2");
+            //Console.WriteLine("foreach eksempel 2");
 
-            string navn = "Ragnar";//vi lager en variabel av typen string og gir den verdien Ragnar
-            foreach (char bokstav in navn)//char står for et tegn/bokstav. Så dette betyr for hvert tegn i Ragnar
-            {
-                Console.WriteLine("value of item: " + bokstav);
-            }
+            //string navn = "Ragnar";//vi lager en variabel av typen string og gir den verdien Ragnar
+            //foreach (char bokstav in navn)//char står for et tegn/bokstav. Så dette betyr for hvert tegn i Ragnar
+            //{
+            //    Console.WriteLine("value of item: " + bokstav);
+            //}
 
             //Sånn, da er det nok eksempler for å gjøre oppgaver.
 
@@ -108,8 +109,22 @@ namespace LabLoops
 
             //Oppgave 2
             //Lag en for loop som skriver ut 5 stykk av dette tegnet *
+            Console.WriteLine("oppgave 1");
+            Console.WriteLine("Skriv inn ditt navn:");
+            string navn = Console.ReadLine();
+            foreach (char bokstav in navn)
+            {
+                Console.WriteLine("value of item: " + bokstav);
+            }
 
 
+            Console.WriteLine("oppgave 2");
+            int start = 0;
+            int stopp = 5;
+            for (int i = start; i < stopp; i++)
+            {
+                Console.WriteLine("*");
+            }
             Console.ReadLine();
         }
     }
